debebc389c6f998d94a3eb3dbcaf8a0b2014d1cf
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2024-04-13 17:31:45 +0530

    Create 25.cpp

---
+++ Pattern/25.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int n;
+   cin>>n;
+   int i=1,num=1;
+   while(i<=n){
+      int j=1,space=1;
+      while(space<=n-i){
+         cout<<" ";
+         space++;
+      }
+      while(j<=i){
+         cout<<num<<" ";
+         j++;
+         num++;
+      }
+      i++;
+      cout<<endl;
+   }
+   return 0;
+}
+
