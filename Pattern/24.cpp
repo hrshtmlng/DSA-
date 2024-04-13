dfa24b400325a233e7e956b9aa5e7e000b7a67b4
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2024-04-13 17:00:11 +0530

    Create 24.cpp

---
+++ Pattern/24.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int n;
+   cin>>n;
+   int i =1;
+   
+   while(i<=n){
+      int j=1,space=1;
+      while(space<=n-i){
+         cout<<" ";
+         space++;
+      }
+      
+      while(j<=i){  
+         cout<<i;
+     
+         j++;
+      }
+      cout<<endl;
+  
+      i++;
+   }
+   return 0;
+}
+
+
+
